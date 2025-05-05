0fa47e5c6fde71beb26f036e997bb693af01f15c
Serena <serefisch17@gmail.com>
2025-05-04 21:48:16 -0300

    .

--- src/ejercicios/EscuelaIngles.java
+++ src/ejercicios/EscuelaIngles.java
@@ -3,12 +3,39 @@ package ejercicios;
 import java.util.Scanner;
 
 public class EscuelaIngles {
-  public static void main(String[] args) {
+  public static void 10main(String[] args) {
 
     Scanner sc = new Scanner(System.in);
 
+    //Declaro las variables.
+    int edad;
 
+    System.out.println("***** Bienvenido *****");
+    System.out.println("Ingrese la edad de su hijo");
+    edad = sc.nextInt();
+
+    //Comienzo a armar el condicional multiple.
+
+    if (edad >= 4 && edad <= 6){
+      System.out.println("El horario del grupo Kinder es de: Lunes y Miercoles de 16 a 17hs.");
+    }
+    else{
+      if (edad >=7 && edad <=8 ){
+        System.out.println("El horario del grupo 1st Year es de: Martes y Jueves de 16:30 a 17:30hs");
+      }
+      else{
+        if (edad >= 9 && edad <=10){
+          System.out.println("El horario del grupo 2nd Year es de: Martes y Jueves 17:30 a 19hs");
+        }
+        else {
+          if (edad >=11 && edad <=13){
+            System.out.println("El horario del grupo 3rd Year es de: Lunes y Miercoles de 17 a 18:30hs");
+          }
+          else {
+            System.out.println("Edad no correspondiente al rango de edades de la escuela.");
+          }
+        }
+      }
+    }
   }
-
-
 }
